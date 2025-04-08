ae84d5bde6a7e68ae2dae44c880ee978e920fa90
Ahsan Ali <ahsan326ali@gmail.com>
2025-04-08 23:35:03 +0500

    Reapply "Feat: Changes"
    This reverts commit 8abcc9a69e63d83607fe664dcd1a75d1ad89f201.

---
+++ .history/src/assets/mainImages_20250408221810.js
@@ -0,0 +1,4 @@
+export {default as creative1} from "./Images/creative1.jpg"
+export {default as creative2} from "./Images/creative2.jpg"
+export {default as target} from "./Images/target.svg"
+export {default as rocket} from "./Images/rocket.svg"
---
+++ .history/src/assets/mainImages_20250408224558.js
@@ -0,0 +1,5 @@
+export {default as creative1} from "./Images/creative1.jpg"
+export {default as creative2} from "./Images/creative2.jpg"
+export {default as target} from "./Images/target.svg"
+export {default as rocket} from "./Images/rocket.svg"
+// export {default as techryptLogo} from "./Images/techryptLogo.png"
